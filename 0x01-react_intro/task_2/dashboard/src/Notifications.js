be0a03dc7a0db9f35cb7c7f0ad2ed75ae0eef476
Mafika <rudolphmafika@gmail.com>
2024-06-28 12:54:57 +0200

    taks 3

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -5,7 +5,6 @@ export function Notifications() {
     return (
         <div className="Notifications">
             <p>Here is the list of notifications</p>
-            <button style="float: left, ex"></button>
         </div>
     );
 }
